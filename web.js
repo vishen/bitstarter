fcdcbea715afc41eea882a12b54ee766b0e3fa1c
Jonathan Pentecost <pentecostjonathan@gmail.com>
2013-07-05 13:36:50 +1000

    read from index.html

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require('fs')
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  // If no options specified returns buffer which is prefered as
+  // buffer deals in binary data, not string,.
+  var buffer = fs.readFileSync('index.html');
+  response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
